637cf12b97bf265b68d67fe88112b65041a9bec5
Mike Minckler <mikeminckler@gmail.com>
2017-08-11 02:24:30 +0000

    added flavours and gallery pics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,8 +37,10 @@
 
             <div class="flexbox">
 
-                <img src="/images/original.jpg">
-                <div>
+                <div class="flex-3">
+                    <img src="/images/original.jpg" class="square inline">
+                </div>
+                <div class="flex-2 flexbox flex-column">
                     <h3>Original</h3>
                     <ul>
                         <li>Green Salad</li>
@@ -55,9 +57,61 @@
         </div>
 
 
+        <div class="p">
+
+            <div class="flexbox">
+
+                <div class="flex-3">
+                    <img src="/images/ginger.jpg" class="square inline">
+                </div>
+                <div class="flex-2 flexbox flex-column">
+                    <h3>Ginger Seasme</h3>
+                    <ul>
+                        <li>Rice</li>
+                        <li>Sushi</li>
+                        <li>Stirfry</li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="p">
+
+            <div class="flexbox">
+
+                <div class="flex-3">
+                    <img src="/images/chili.jpg" class="square inline">
+                </div>
+                <div class="flex-2 flexbox flex-column">
+                    <h3>Chili</h3>
+                    <ul>
+                        <li>Beef</li>
+                        <li>Chicken</li>
+                        <li>Seafood</li>
+                        <li>Marinade</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+
         <p>
-            <img src="images/yeshi-bottles.jpg">
+            <div class="gallery">
+
+                <div class="image"><img src="/images/gallery/salad.jpg"></div>
+                <div class="image"><img src="/images/gallery/sushi.jpg"></div>
+                <div class="image"><img src="/images/gallery/stirfry.jpg"></div>
+                <div class="image"><img src="/images/gallery/egg.jpg"></div>
+                <div class="image"><img src="/images/gallery/steak.jpg"></div>
+                <div class="image"><img src="/images/gallery/bowl.jpg"></div>
+                <div class="image"><img src="/images/gallery/shrimp.jpg"></div>
+                <div class="image"><img src="images/yeshi-bottles.jpg"></div>
+
+            </div>
         </p>
+
 
     </div>
 
